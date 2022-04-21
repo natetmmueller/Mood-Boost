c19a584980101945b098e585486897adf511c3ae
Nate Mueller <natemueller2@gmail.com>
2022-04-21 13:16:01 -0400

    fixing the backend controllers

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -20,11 +20,14 @@ exports.post_index = (req, res) => {
 
 //post a post 
 exports.addPost = (req, res) => {
-    req.body.user = req.user.firstName
-    Post.create(req.body)
-    
-    //this is going to have to show up
-    res.status(200).send('Done')
+    try{
+        req.body.user = req.user.firstName
+        Post.create(req.body)
+        res.status(200).send('Done')
+
+    } catch(err) {
+        res.status(400).json(err);
+    }
 }
 
 
@@ -33,30 +36,45 @@ exports.showPost = async (req, res) => {
     try {
         let post = await Post.findById(req.params.id).populate('user');
         res.status(200).json(post);
-      } catch (err) {
+
+      } catch(err){
         res.status(400).json(err);
       }
 }
 
 //add a comment
 exports.addComment = (req, res) => {
-    req.body.user = req.user
-    let post = Post.findById(req.params.id)
-    post.comments.push(req.body)
-    post.save()
-    res.status(200).send('Done')
-    
+    try {
+        req.body.user = req.user
+        let post = Post.findById(req.params.id)
+        post.comments.push(req.body)
+        post.save()
+        res.status(200).send('Done')
+
+    } catch(err){
+        res.status(400).json(err);
+    }
 }
 
 exports.postEdit = (req, res) => {
-   let update = Post.findByIdAndUpdate(req.body.id, req.body)
-   res.status(200).send('Done')
+    try{
+        let update = Post.findByIdAndUpdate(req.body.id, req.body)
+        res.status(200).send('Done')  
+
+    } catch(err) {
+        res.status(400).json(err);
+    }
 }
 
 
 //deleting the post
 exports.deletePost = (req, res) => {
-    Post.findByIdAndDelete(req.params.id)
-    res.status(200).send('Done') 
+    try {
+        Post.findByIdAndDelete(req.params.id)
+        res.status(200).send('Done') 
+
+    } catch(err){
+        res.status(400).json(err);
+    }
 }
 
