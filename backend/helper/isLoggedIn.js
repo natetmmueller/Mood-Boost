6081eec88f7a84c36a26835b724787db035d2fc8
Nicole Broughton <nbrought28@gmail.com>
2022-04-21 11:18:35 -0700

    Complete Auth

---
+++ backend/helper/isLoggedIn.js
@@ -0,0 +1,27 @@
+// Middleware for user to check
+
+// Middleware for user to check.
+module.exports = (req, res, next) => {
+  // const token = req.header("x-auth-token");
+  var authorizationToken = req.header("Authorization");
+  console.log(authorizationToken);
+  authorizationToken = authorizationToken.replace("Bearer ", "")
+  const token = authorizationToken
+
+  if (!token) {
+    return res
+      .json({
+        message: "Aha! You're not allowed to view this!",
+      })
+      .status(401);
+  }
+  try{
+    const decoded = jwt.verify(token, process.env.secret)
+  }
+
+  if (!req.user) {
+    res.redirect("/auth/signin");
+  } else {
+    next();
+  }
+};
