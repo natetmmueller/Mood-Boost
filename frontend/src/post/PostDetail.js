aa5d08cc89e762a30a2d0aa0d76af566a134cd24
Nate Mueller <natemueller2@gmail.com>
2022-04-25 17:34:50 -0400

    detail page works

--- frontend/src/post/PostDetail.js
+++ frontend/src/post/PostDetail.js
@@ -1,35 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 import Axios from 'axios'
 import { useParams } from 'react-router'
 
 export default function PostDetail(props) {
     const params = useParams()
+
+    const [post,setPost] = useState(null)
+
+
     console.log(params.id)
+
+    useEffect(() => {
+        Axios.get(`/post/?id=${params.id}`).then((response) => {
+            console.log(response.data)
+            setPost(response.data)
+            console.log(post,"post")
+          })
+            .catch((error) =>{
+              console.log(error)
+            
+          })
+
+    },[])
+    // let data; 
     
-        Axios.get(`http://localhost:3002/post/?id=${params.id}`).then((response) => {
-          console.log(response.data)
-        })
-          .catch((error) =>{
-            console.log(error)
-          
-        })
-    
+    // Axios.get(`/post/?id=${params.id}`).then((response) => {
+    //     console.log(response.data)
+    //     //  setPost(await response.data)
+    //      data = response.data
+    //     console.log(post,"post")
+    //   })
+    //     .catch((error) =>{
+    //       console.log(error)
+        
+    //   })
       
   return (
     <Container>
-        <>
-            <td><b>{this.props.title}</b> makes me happy!</td>
+        <> {post&&
+        <div>
+            <td><b>{post.postTitle}</b> makes me happy!</td>
             <br/>
-            <td> It makes me <b>{props.scale}</b>/10 happy!</td>
+            <td> It makes me <b>{post.scale}</b>/10 happy!</td>
             <br/>
-            <td> why does it make me happy? <b>{props.description}</b></td>
+            <td> why does it make me happy? <b>{post.description}</b></td>
             <br/>
-            <td> Click here to see it: <b>{props.link}</b></td>
+            <td> Click here to see it: <b>{post.linkToIt}</b></td>
             <br/>
             <br/>
             Comments
-            <br/>  
+            <br/> 
+        </div>
+        } 
         </>
     </Container>
   )
