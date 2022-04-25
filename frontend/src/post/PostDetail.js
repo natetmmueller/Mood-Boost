02a2fc2378787cc4abed1c776d94f109618e6e57
Nate Mueller <natemueller2@gmail.com>
2022-04-25 18:03:43 -0400

    got rid of details commented out code

--- frontend/src/post/PostDetail.js
+++ frontend/src/post/PostDetail.js
@@ -23,18 +23,7 @@ export default function PostDetail(props) {
           })
 
     },[])
-    // let data; 
     
-    // Axios.get(`/post/?id=${params.id}`).then((response) => {
-    //     console.log(response.data)
-    //     //  setPost(await response.data)
-    //      data = response.data
-    //     console.log(post,"post")
-    //   })
-    //     .catch((error) =>{
-    //       console.log(error)
-        
-    //   })
       
   return (
     <Container>
@@ -59,65 +48,3 @@ export default function PostDetail(props) {
 }
 
 
-// export default class PostDetail extends Component {
-//     constructor(props) {
-//       super(props)
-    
-//       console.log(props,"line 9 PD")
-//     //   console.log(props.params,"this is the props.params")
-//     //   console.log(props.match.params.id)
-//      let url = new URLSearchParams(window.location.search).get('id')
-//      console.log(url, "url")
-//     }
-
-//     componentDidMount(){
-//         console.log(this.props,"this.props")
-//         this.showPostDetail(this.props.match.params.id)
-//     }
-    
-//     state = {
-//         title: this.props.postTitle,
-//         scale: this.props.scale,
-//         description: this.props.description,
-//         link: this.props.linkToIt
-//       };
-
-//       showPostDetail = (id) => {
-//         Axios.get(`post?id=${id}`).then((response) => {
-//           console.log(response.data)
-//         })
-//           .catch((error) =>{
-//             console.log(error)
-          
-//         })
-//       }
-    
-
-
-//   render() {
-//       console.log(this.props.match.params, "match.params")
-//     return (
-//       <Container>
-//           <>
-          
-                  
-//                       <td><b>{this.state.title}</b> makes me happy!</td>
-//                       <br/>
-//                       <td> It makes me <b>{this.state.scale}</b>/10 happy!</td>
-//                       <br/>
-//                       <td> why does it make me happy? <b>{this.state.description}</b></td>
-//                       <br/>
-//                       <td> Click here to see it: <b>{this.state.link}</b></td>
-//                       <br/>
-//                       <br/>
-//                       Comments:
-//                       <br/>
-                      
-                      
-                  
-              
-//           </>
-//       </Container>
-//     )
-//   }
-// }
